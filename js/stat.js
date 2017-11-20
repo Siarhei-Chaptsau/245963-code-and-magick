ecad2915bfea8cd77b6fd92300796c0900f17dff
Siarhei on work <s.chaptsau@gmail.com>
2017-11-20 16:46:23 +0300

    Has created stat.js and first code

---
+++ js/stat.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// функция вызывается когда игрок проходит уровень
+window.renderStatistic = function (ctx, names, times) {
+  var names = ['Siarhei'];
+  times.length == names.length;
+
+  this.fillText = function (ctx, names, times) {
+    alert('Ура вы победили!\nСписок результатов:');
+  }
+
+}
