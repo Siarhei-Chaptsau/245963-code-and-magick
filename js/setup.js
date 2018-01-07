104d5ca47ecbce37a3eaa2f3b39e68925fd4ab4c
Siarhei <s.chaptsau@gmail.com>
2018-01-07 20:20:38 +0300

    добавил функции загрузки и отправки данных

--- js/setup.js
+++ js/setup.js
@@ -1,79 +1,60 @@
 'use strict';
 
 (function () {
-  var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон']; // Массив имён магов
-  var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг']; // Массив фамилий магов
-  var WIZARD_COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)']; // Массив цветов плащей магов
-  var WIZARD_EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green']; // Массив цветов очей магов
-  var WIZARD_FIREBALL_COLOR = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848']; // Массив цветов фаерболла
-
   var setup = document.querySelector('.setup');
-  var wizardCoat = setup.querySelector('.setup-wizard .wizard-coat');
+  /* var wizardCoat = setup.querySelector('.setup-wizard .wizard-coat');
   var wizardEyes = setup.querySelector('.setup-wizard .wizard-eyes');
-  var wizardFireball = setup.querySelector('.setup-fireball-wrap');
+  var wizardFireball = setup.querySelector('.setup-fireball-wrap');*/
 
   var similarListElement = setup.querySelector('.setup-similar-list'); // находим элемент, в который мы будем вставлять похожих магов
   var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item'); // Находим шаблон-мага, который мы будем копировать (мы используем весь DocumentFragment, находящийся в шаблоне)
 
-  /* var getRandomNumber = function (min, max) { // функция рандома возвращает случайное число в заданном пределе
-    return Math.round(Math.random() * (max - min) + min);
-  }; */
-
-  // функция генерации случайных элементов массива
-  var getRandom = function (arr) {
-    var randomIndex = Math.round(Math.random() * (arr.length - 1)); // получаем случайное число от 0 до индекса последнего элемента
-    return arr[randomIndex];
-  };
-
-  // показываем окно настроек пользователя
-  setup.classList.remove('hidden');
-
-  // Массив магов с именами и цветами
-  var wizards = [
-    {
-      name: getRandom(WIZARD_NAMES) + ' ' + getRandom(WIZARD_SURNAMES),
-      coatColor: getRandom(WIZARD_COAT_COLOR),
-      eyesColor: getRandom(WIZARD_EYES_COLOR)
-    },
-    {
-      name: getRandom(WIZARD_NAMES) + ' ' + getRandom(WIZARD_SURNAMES),
-      coatColor: getRandom(WIZARD_COAT_COLOR),
-      eyesColor: getRandom(WIZARD_EYES_COLOR)
-    },
-    {
-      name: getRandom(WIZARD_NAMES) + ' ' + getRandom(WIZARD_SURNAMES),
-      coatColor: getRandom(WIZARD_COAT_COLOR),
-      eyesColor: getRandom(WIZARD_EYES_COLOR)
-    },
-    {
-      name: getRandom(WIZARD_NAMES) + ' ' + getRandom(WIZARD_SURNAMES),
-      coatColor: getRandom(WIZARD_COAT_COLOR),
-      eyesColor: getRandom(WIZARD_EYES_COLOR)
-    }
-  ];
+  setup.classList.remove('hidden'); // показываем окно настроек пользователя
 
   // Отрисуем шаблон-мага - функция создания DOM-элемента на основе JS-объекта
   var renderWizard = function (wizard) {
     var wizardElement = similarWizardTemplate.cloneNode(true); // клонируем содержимое шаблон-мага
     wizardElement.querySelector('.setup-similar-label').textContent = wizard.name; // добавляем имя в шаблон-мага
-    wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor; // добавляем цвет плаща в шаблон-мага
-    wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor; // добавляем цвет глаз мага в шаблон-мага
+    wizardElement.querySelector('.wizard-coat').style.fill = wizard.colorCoat; // добавляем цвет плаща в шаблон-мага
+    wizardElement.querySelector('.wizard-eyes').style.fill = wizard.colorEyes; // добавляем цвет глаз мага в шаблон-мага
     return wizardElement;
   };
 
-  var fragment = document.createDocumentFragment();
-  for (var i = 0; i < wizards.length; i++) {
-    fragment.appendChild(renderWizard(wizards[i]));
-  }
+  // Функция загрузки данных при успешном результате
+  var successHandler = function (wizards) {
+    var fragment = document.createDocumentFragment();
+    for (var i = 0; i < 4; i++) { // отображать-вставлять будем только 4 мага из загруженных
+      fragment.appendChild(renderWizard(wizards[i]));
+    }
+    similarListElement.appendChild(fragment); // вставляем отрисованного мага в нужное поле
+    setup.querySelector('.setup-similar').classList.remove('hidden'); // показываем блок с похожими персонажами
+  };
 
-  // вставляем склонированного шаблон-мага в нужное поле
-  similarListElement.appendChild(fragment);
-  // показываем блок с похожими персонажами
-  document.querySelector('.setup-similar').classList.remove('hidden');
+  // Функция вывода ошибки при отправке
+  var errorHandler = function (errorMessage) {
+    var node = document.createElement('div');
+    node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red;';
+    node.style.position = 'absolute';
+    node.style.left = 0;
+    node.style.right = 0;
+    node.style.fontSize = '30px';
+    node.textContent = errorMessage;
+    document.body.insertAdjacentElement('afterbegin', node);
+  };
+  window.backend.load(successHandler, errorHandler);
+
+  var form = setup.querySelector('.setup-wizard-form');
+  form.addEventListener('submit', function (evt) {
+    window.backend.save(new FormData(form), function () {
+      setup.classList.add('hidden');
+    }, errorHandler);
+    evt.preventDefault();
+  });
 
   // ----- использование callbacks ----- //
 
   // Функция изменения цвета
+  /*
   var fillElement = function (element, color) {
     element.style.fill = color;
   };
@@ -100,6 +81,7 @@
     // fireball.style.background = getRandom(WIZARD_FIREBALL_COLOR);
     window.colorizeElement(wizardFireball, getRandom(WIZARD_FIREBALL_COLOR), changeElementBackground);
   });
+  */
 
   // ----- Drag and drop ----- //
 
